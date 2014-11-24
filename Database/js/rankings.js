b5794aa4c58b726d8e69eb2877db881e090fdd04
rmukerji <rmukerj2@illinois.edu>
2014-11-24 14:31:54 -0800

    Nov 24

--- Database/js/rankings.js
+++ Database/js/rankings.js
@@ -3,13 +3,11 @@ $(document).ready(function(){
 	for(var key in data){
 		var row;
 		if(name == data[key]['username'])
-			row = '<tr bgcolor="#FFFF00"><td>' + count + "</td><td>" + data[key]['username'] + "</td><td>" + data[key]['Points'] + "</td></tr>";
+		row = '<tr bgcolor="#FFFF00"><td>' + count + "</td><td>" + data[key]['username'] + "</td><td>" + data[key]['Points'] + "</td></tr>";
 		else{
-			row = '<tr><td>' + count + "</td><td><a data-toggle='modal' data-id='" + data[key]['username'] + "' class='open-AddBookDialog' href='#addBookDialog'>" + data[key]['username'] + "</a></td><td>" + data[key]['Points'] + "</td></tr>";
-			console.log(row);
+			row = '<tr><td>' + count + "</td><td><a href='show_players.php?players_for=" + data[key]['username'] + "'>" + data[key]['username'] + "</a></td><td>" + data[key]['Points'] + "</td></tr>";
 		}
 		$("#mainTable").append(row);
-		count++;
+			count++;
 	}
 });
-
